4ec23e9203ae86aa02a55705b8ac66da5de7212f
jchrys <jchrys@me.com>
2019-08-11 21:32:34 +0900

    :construction: Vector.cpp

--- docs/DataStructures/Basics/Vector.cpp
+++ docs/DataStructures/Basics/Vector.cpp
@@ -1,66 +1,52 @@
 template<typename T>
 class Vector {
+    unsigned int _size;
+    unsigned int _capacity;
+    T* _buf;
     public:
-        int _size;
-        int capacity;
-        T *arr;
-        Vector() {
-            _size = 0;
-            capacity = 32;
-            arr = new T[capacity];
-        }
-        Vector(int k) {
-            _size = k;
-            capacity = k;
-            arr = new T[capacity];
-        }
-        ~Vector() {
-            delete[] arr;
-        }
+    // capacity: 
+    unsigned int size() {
+        return _size;
+    }
 
-        void clear() {
-            delete[] arr;
-            _size = 0;
-            capacity = 32;
-            arr = new T[capacity];
-        }
-        void resize(int k) { //TODO smaller!
-            T *temp;
-            temp = new T[k];
-            for (int i = 0; i < _size; i++)
-                temp[i] = arr[i];
-            delete[] arr;
-            arr = temp;
-            capacity = k;
-            _size = (_size < capacity ? _size: capacity); 
-        }
+    void resize(unsigned int size) {
+        //TODO
+        _size = size;
+    }
 
-        int size() const {
-            return _size;
-        }
-        T* begin() const {
-            return &arr[0];
-        }
-        T* end() const {
-            return &arr[0] + _size;
-        }
+    unsigned int capacity();
+    bool empty();
+    // unsigned int max_size();
+    // reverse()
+    // shrink_to_fit()
+   
 
-        void push_back(T val) {
-            if (_size == capacity) {
-                resize(_size*2);
-            }
-            arr[_size++] = val;
-        }
+    //element access:
+    T back();
+    // operator[]()
+    // at()
+    // front()
+    // data()
+   
 
-        void pop_back() {
-            _size--;
-        }
-
-        T& operator[](int index) {
-            return arr[index];
-        }
-
-        T operator [] (int index) const {
-            return arr[index];
-        }
+    //Modifiers
+    void clear();
+    void push_back(T val);
+    void pop_back();
+    // assign()
+    // insert()
+    // erase()
+    // emplace()
+    // emplace_back
+   
+    
+    //Iterators 
+    T* begin();
+    T* end();
+    //T* rbegin();
+    //T* rend();
+    //T* const cbegin();
+    //T* const cend();
+    //T* const crbegin();
+    //T* const crend();
 };
